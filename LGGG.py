4bb2496df1cf99a8c5230c44d6a30b405cef8b1c
yoonyouth <yoon.youth.cs@gmail.com>
2021-07-07 08:55:15 +0900

    creating runGame func

--- LGGG.py
+++ LGGG.py
@@ -3,3 +3,18 @@ import pygame
 WHITE = (255,255,255)
 pad_width = 2048
 pad_height = 2048
+
+def runGame():
+    global gamepad
+
+    empty = False
+    while not empty:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                empty = True
+
+        gamepad.fill(WHITE)
+        pygame.display.update()
+
+    pygame.quit()
+
