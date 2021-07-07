bb521d3e95773a3ff97b0a9624ffd10a0e6b93ee
yoonyouth <yoon.youth.cs@gmail.com>
2021-07-08 08:34:33 +0900

    random func

---
+++ Random.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+        var lists = new Array('yoon', 'yoon_old', 'yunjeolmeum', 'yoon_youth', 'yoon_german');
+        function randomItem(a){
+            var result = a[Math.floor(Math.random() * a.length)];
+            return result;
+        }
+        randomItem(list);
+</script>
