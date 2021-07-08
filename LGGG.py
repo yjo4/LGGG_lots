8f3aaaaf024cd7995012fe08d365710c0449f167
yoonyouth <yoon.youth.cs@gmail.com>
2021-07-08 20:24:13 +0900

    pygame update

--- LGGG.py
+++ LGGG.py
@@ -1,30 +1,104 @@
 import pygame
 
-WHITE = (255,255,255)
-pad_width = 2048
-pad_height = 2048
+#1. 뽑기 초기화
+pygame.init()
 
-def runGame():
-    global gamepad
+#2. 뽑기 옵션 설정
+size = [400, 400]
+screen = pygame.display.set_mode(size)
 
-    empty = False
-    while not empty:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                empty = True
+title = "LGGG lots" #직접 입력 받기
+pygame.display.set_caption(title)
 
-        gamepad.fill(WHITE)
-        pygame.display.update()
+#3. 스크린 설정
+clock = pygame.time.Clock()
 
-    pygame.quit()
+img1 = pygame.image.load("images/IMG1.PNG").convert_alpha()
+img1 = pygame.transform.scale(img1,(400,400))
 
-def initGame():
-    global gamepad
+img2 = pygame.image.load("images/IMG2.PNG").convert_alpha()
+img2 = pygame.transform.scale(img2,(400,400))
+
+img3 = pygame.image.load("images/IMG3.PNG").convert_alpha()
+img3 = pygame.transform.scale(img3,(400,400))
+
+k = 0
+
+#4. 메인 이벤트 
+
+SB = 0
+while SB == 0:
+
+    # 4-1. FPS 설정
+    clock.tick(5)
+
+    # 4-2. 각종 입력 감지
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            SB = 1
+
+    # 4-3. 입력, 시간에 따른 변화
+    k += 1
+    if k % 2 == 0:
+        screen.blit(img1,(0,0))
+    else:
+        screen.blit(img2,(0,0))
+
+    # 4-4. 그리기
+    #screen.fill(white)
+    # screen.blit(img1,(0,0))
+
+    # 4-5. 업데이트
+    pygame.display.flip()
     
-    pygame.init()
-    gamepad = pygame.display.set_mode((pad_width, pad_height))
-    pygame.display.set_caption('LGGG Lots')
+    # name = 12
+    # while name != 0:
 
-    runGame()
+    #     #4-1. 뽑기 종료
+        
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             name = 0
+        
+    #     #4-2. 숨기기
+        
+    #     #4-3. 남은 사람 목록
+        
+    #     #4-4. 배경
+    #     screen.fill(LGGGImg)
+        
+    #     #4-5. 업데이트
+    #     pygame.disply.flip()
+    
+#5. 종료
+pygame.quit()
+# import pygame
 
-initGame()
+# WHITE = (255,255,255)
+# pad_width = 2048
+# pad_height = 2048
+
+# def runGame():
+#     global gamepad
+
+#     empty = False
+#     while not empty:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 empty = True
+
+#         gamepad.fill(WHITE)
+#         pygame.display.update()
+
+#     pygame.quit()
+
+# def initGame():
+#     global gamepad
+    
+#     pygame.init()
+#     gamepad = pygame.display.set_mode((pad_width, pad_height))
+#     pygame.display.set_caption('LGGG Lots')
+
+#     runGame()
+
+# initGame()
